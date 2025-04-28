27ab2d4fa8c667c7b97d980ce445c83cf3a6acfd
leeyoungkon <yklee2002@gmail.com>
2025-04-28 13:25:37 +0900

    Add files via upload

---
+++ book-ui2/src/main/java/com/example/demo/entity/Book.java
@@ -0,0 +1,23 @@
+package com.example.demo.entity;
+
+
+import lombok.Data;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String author;
+    private LocalDateTime published;
+}
