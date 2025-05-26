4bce0a5989f8717ca1ee02a2bc1d2067a0fa3b1c
leeyoungkon <yklee2002@gmail.com>
2025-05-27 01:10:41 +0900

    Delete app0505 directory

--- app0505/app/src/main/java/com/tuk/app0505/ApiService.java
+++
@@ -1,9 +0,0 @@
-package com.tuk.app0505;
-import retrofit2.Call;
-import retrofit2.http.GET;
-import retrofit2.http.Path;
-
-public interface ApiService {
-    @GET("/book/{id}")
-    Call<Book> getBook(@Path("id") Long id);
-}
